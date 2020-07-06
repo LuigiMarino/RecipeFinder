dffdaa5e1715e09e993db0e828005f5cdb8e819f
Luigi <emails@luigi-marino.com>
2020-07-06 12:30:28 +0100

    Get ingredients

--- recipe.py
+++ recipe.py
@@ -11,12 +11,28 @@ today = date.today()
 
 #Get Website
 r1 = requests.get("https://spoonacular.com/")
-page = BeautifulSoup(r1.text, 'html.parser')
+page1 = BeautifulSoup(r1.text, 'html.parser')
 
 #Get Recipe Of The Day
-rotd = page.find('div', attrs={'id': re.compile('recipeOfTheDay')})
-children = rotd.findChildren('a', recursive=False)
+rotdDiv = page1.find('div', attrs={'id': re.compile('recipeOfTheDay')})
+rotdA = rotdDiv.findChildren('a', recursive=False)
 
 #Cycle Through And Print href
-for a in children[0:1]: 
-    print("https://spoonacular.com{}".format(a.get('href')))
+for a in rotdA[0:1]: 
+    rotdLink = ("https://spoonacular.com{}".format(a.get('href')))
+    print(rotdLink)
+
+r2 = requests.get(rotdLink)
+page2 = BeautifulSoup(r2.text, 'html.parser')
+
+ingredientsList = page2.find_all('div', attrs={'class': 'spoonacular-ingredient-list'})
+#ingredientsName = ingredientsList.findChildren('div', attrs={'class' : 'spoonacular-name'})
+
+i = 1
+for a in ingredientsList:
+    ingredientsName = a.findChildren('div', attrs={'class' : 'spoonacular-name'})
+    ingredientsAmount = a.findChildren('div', attrs={'class' : 'spoonacular-amount spoonacular-metric'})
+    for b in ingredientsName:
+        print(b.text)
+    #print(ingredientsAmount.contents)
+    i += 1
